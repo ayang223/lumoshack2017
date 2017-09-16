e55a944069b8e07d50cf6b3f66146d6e32b93dbf
clai <clai-@hotmail.com>
2017-09-16 16:03:03 -0700

    initial timer build with working log in

---
+++ app/src/main/java/com/example/clai/lumos/Timer.java
@@ -0,0 +1,30 @@
+package com.example.clai.lumos;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.widget.Button;
+import android.widget.Toast;
+import android.view.*;
+
+public class Timer extends BroadcastReceiver
+{
+//    Button button = (Button) findViewById(R.id.button);
+    @Override
+    public void onReceive(Context context, Intent intent)
+    {
+        Toast.makeText(context, "Alarm! Wake up! Wake up!", Toast.LENGTH_LONG).show();
+        Uri alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+        if (alarmUri == null)
+        {
+            alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        }
+        Ringtone ringtone = RingtoneManager.getRingtone(context, alarmUri);
+        ringtone.play();
+
+        ringtone.stop();
+    }
+}
